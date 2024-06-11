dc8c7cf6c189416ee6cb0e3b761aecf2e06909a2
johndeniel <johndeniel.d37@gmail.com>
2024-06-11 10:29:11 +0800

    chore: add comment for route local development and production

--- src/router/routes.rs
+++ src/router/routes.rs
@@ -10,6 +10,11 @@ pub fn app() -> Router {
 }
 
 async fn serve_html() -> impl IntoResponse {
+    // For production in a Docker image, read the HTML file from the deployed location
     let html_content = fs::read_to_string("/usr/local/bin/static/index.html").unwrap();
+
+    // For local development, read the HTML file from the source directory
+    // let html_content = fs::read_to_string("src/static/index.html").unwrap();
+
     Html(html_content)
 }
